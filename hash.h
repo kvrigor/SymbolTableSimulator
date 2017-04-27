6033601e0391c314c8689e038157d95d30773514
chesterlbtan <chesterlbtan@gmail.com>
2017-04-27 23:11:13 +0800

    Initial Release for Hash functions
    Layout methods of basic Hash
    
    Makefile.win was changed due to different version of Dev-C++ used.
    Please revert changes if needed.

--- hash.h
+++ hash.h
@@ -2,7 +2,11 @@
 #ifndef HASH_H_
 #define HASH_H_
 
+#include <iostream>
 #include "search.h"
+
+using std::cout;
+using std::string;
 
 namespace Hash
 {
@@ -13,7 +17,58 @@ namespace Hash
 		std::vector<Symbol> sampleVector;
 		std::list<Symbol> sampleList;
 	}
-
+	
+	class HashTable
+	{
+		private:
+			std::list<Symbol> _symList;
+			std::vector<Symbol> _symVector;
+			int _tblSize;
+			
+			int hash(Symbol sym);
+			
+		public:
+			HashTable();
+			HashTable(const std::list<Symbol> &);
+			
+	};
+	
+	HashTable::HashTable()
+	{
+		_symList.push_back(Symbol());
+		_symVector.push_back(Symbol());
+		_tblSize = 1;
+	}
+	
+	HashTable::HashTable(const std::list<Symbol> & lstSymbol)
+	{
+		int count = lstSymbol.size();
+		for(int i = 0; i<count; i++)
+		{
+			
+		}
+	}
+	
+	/*
+	int hash32 (Symbol sym, int tbl_size)
+	{
+		int h = 0;
+		
+		string asd = sym.ToString();
+		std::cout<<"\n"<<sym.Name<<"\t"<<sym.Scope<<"\t"<<sym.Type<<"\t\n"
+			<<asd<<"\n"<<asd.size()<<"\n";
+		
+		return asd.size();
+	}
+	
+	
+	int test(std::list<Symbol> testing)
+	{
+		std::vector<Symbol> testVector(testing.begin(), testing.end());
+		return hash32(testVector[3], 2);
+	}
+	*/
+	
 }
 
 namespace ColRes
